db43009f0b32b323b5b51784a366217731d0f02a
langyo <langyo.china@gmail.com>
2019-10-08 16:56:49 +0800

    .

---
+++ configs/initTheme.js
@@ -0,0 +1,12 @@
+export default {
+    defaultTheme: {
+        primary: '#66CCFF',
+        secondary: '#66CCFF'
+    },
+    themes: [
+        {
+            name: '天依蓝',
+            color: '#66CCFF'
+        }
+    ]
+}
